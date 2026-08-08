bf13cd10e3348098db4d2c76d48147c1c5cfd71b
Praveen7914 <bandikepraveen5@gmail.com>
2026-08-08 15:21:56 +0530

    changes

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.raithupalu.app',
+  appName: 'RaithuPalu',
+  webDir: 'build'
+};
+
+export default config;
